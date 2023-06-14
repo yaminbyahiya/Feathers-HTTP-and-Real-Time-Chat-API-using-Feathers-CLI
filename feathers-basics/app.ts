789e4e2532e51250c853c90a5fae3976575eaa84
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-06-14 14:10:25 +0600

    Web Socket & KoaJS Integration

--- feathers-basics/app.ts
+++ feathers-basics/app.ts
@@ -1,5 +1,7 @@
 import { feathers } from '@feathersjs/feathers';
 import { text } from 'stream/consumers';
+import { koa, rest, bodyParser, errorHandler, serveStatic } from '@feathersjs/koa'
+import socketio from '@feathersjs/socketio'
 
 interface Message {
     id?:number
@@ -25,24 +27,44 @@ type ServiceTypes = {
     messages:MessageService
 }
 
-const app = feathers<ServiceTypes>();
+const app = koa<ServiceTypes>(feathers());
+
+app.use(serveStatic('.'));
+
+app.use(errorHandler());
+
+app.use(bodyParser());
+
+app.configure(rest());
+
+app.configure(socketio());
 
 app.use('messages', new MessageService());
 
-app.service('messages').on('created', (message:Message) => {
-    console.log('A message has been created', message);
+app.on('connection', (connection) => app.channel('everybody').join(connection));
+
+app.publish((_data) => app.channel('everybody'));
+
+app.listen(3030).then(()=>console.log('Feathers server listening on localhost:3030'));
+
+// app.service('messages').on('created', (message:Message) => {
+//     console.log('A message has been created', message);
+// })
+
+// const main = async() => {
+//     await app.service('messages').create({
+//         text:'Hello Feathers'
+//     })
+
+//     await app.service('messages').create({
+//         text:'Hello again'
+//     })
+//     const messages = await app.service('messages').find();
+//     console.log('All messages', messages);
+// }
+
+// main();
+
+app.service('messages').create({
+    text:'Hello world from the server'
 })
-
-const main = async() => {
-    await app.service('messages').create({
-        text:'Hello Feathers'
-    })
-
-    await app.service('messages').create({
-        text:'Hello again'
-    })
-    const messages = await app.service('messages').find();
-    console.log('All messages', messages);
-}
-
-main();
