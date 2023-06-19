d3477e3fb17c541de0fe8ec909cf325e61c909d6
Yamin Bin Yahiya <yaminbyahiya@gmail.com>
2023-06-19 15:49:35 +0600

    Handling messages using service schema

--- feathers-basics/feathers-chat/src/services/messages/messages.schema.js
+++ feathers-basics/feathers-chat/src/services/messages/messages.schema.js
@@ -1,19 +1,27 @@
 // // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
-import { resolve } from '@feathersjs/schema'
+import { resolve, virtual} from '@feathersjs/schema'
 import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
 import { ObjectIdSchema } from '@feathersjs/typebox'
 import { dataValidator, queryValidator } from '../../validators.js'
+import { userSchema } from '../users/users.schema'
 
 // Main data model schema
 export const messageSchema = Type.Object(
   {
     _id: ObjectIdSchema(),
-    text: Type.String()
+    text: Type.String(),
+    createdAt: Type.Number(),
+    userId: Type.String({objectid: true}),
+    user: Type.Ref(userSchema)
   },
   { $id: 'Message', additionalProperties: false }
 )
 export const messageValidator = getValidator(messageSchema, dataValidator)
-export const messageResolver = resolve({})
+export const messageResolver = resolve({
+  user: virtual(async (message, context) => {
+    return context.app.service('users').get(message.userId);
+  })
+})
 
 export const messageExternalResolver = resolve({})
 
@@ -22,7 +30,14 @@ export const messageDataSchema = Type.Pick(messageSchema, ['text'], {
   $id: 'MessageData'
 })
 export const messageDataValidator = getValidator(messageDataSchema, dataValidator)
-export const messageDataResolver = resolve({})
+export const messageDataResolver = resolve({
+  userId: async(_value, _message, _context) => {
+    return context.params.user._id;
+  },
+  createdAt: async() => {
+    return Date.now();
+  }
+})
 
 // Schema for updating existing entries
 export const messagePatchSchema = Type.Partial(messageSchema, {
@@ -32,7 +47,7 @@ export const messagePatchValidator = getValidator(messagePatchSchema, dataValida
 export const messagePatchResolver = resolve({})
 
 // Schema for allowed query properties
-export const messageQueryProperties = Type.Pick(messageSchema, ['_id', 'text'])
+export const messageQueryProperties = Type.Pick(messageSchema, ['_id', 'text', 'createdAt', 'userId'])
 export const messageQuerySchema = Type.Intersect(
   [
     querySyntax(messageQueryProperties),
@@ -42,4 +57,11 @@ export const messageQuerySchema = Type.Intersect(
   { additionalProperties: false }
 )
 export const messageQueryValidator = getValidator(messageQuerySchema, queryValidator)
-export const messageQueryResolver = resolve({})
+export const messageQueryResolver = resolve({
+  useriD: async (value, user, context) => {
+    if(context.params.user && context.method !== 'find'){
+      return context.params.user._id;
+    }
+    return value;
+  }
+})
